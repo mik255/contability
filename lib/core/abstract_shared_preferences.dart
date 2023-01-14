01c8bc79198efaaae39205b3004db81cab496ff0
Mik255 <mikael20informatica@gmail.com>
2023-01-14 14:30:42 -0300

    initial db with share preferences

---
+++ lib/core/abstract_shared_preferences.dart
@@ -0,0 +1,40 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferenceUtils {
+  late SharedPreferences prefs;
+  void init()async{
+     prefs = await SharedPreferences.getInstance();
+  }
+  String? getString(String key){
+     String? value = prefs.getString(key);
+     return value;
+  }
+  int? getInt(String key){
+    int? value = prefs.getInt(key);
+    return value;
+  }
+  bool? getBool(String key){
+    bool? value = prefs.getBool(key);
+    return value;
+  }
+  double? getDouble(String key){
+    double? value = prefs.getDouble(key);
+    return value;
+  }
+
+  void setString(String key,value)async{
+    await prefs.setString(key,value);
+  }
+  void setInt(String key,value)async{
+    await prefs.setInt(key,value);
+  }
+  void setBool(String key,value)async{
+    await prefs.setBool(key,value);
+  }
+  void setDouble(String key,value)async{
+    await prefs.setDouble(key,value);
+  }
+  void setStringList(String key,List<String> value)async{
+    await prefs.setStringList(key,value);
+  }
+}
---
+++ lib/credit_card.dart
@@ -0,0 +1,13 @@
+
+class CreditCart {
+  String name;
+  String imgPath;
+  double balanceLimit;
+  DateTime invoiceClosing;
+  CreditCart({
+    required this.name,
+    required this.imgPath,
+    required this.balanceLimit,
+    required this.invoiceClosing,
+  });
+}
