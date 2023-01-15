221cc3f95e2ad2cdb841b60fad88edd9abdbe9a1
Mik255 <mikael20informatica@gmail.com>
2023-01-14 21:07:13 -0300

    error handle

--- lib/modules/home/store/valueNotifie_home_storie.dart
+++ lib/modules/home/store/valueNotifie_home_storie.dart
@@ -1,28 +1,39 @@
-
-
-
 import 'package:contability/entities/credit_card.dart';
+import 'package:contability/entities/errorModel.dart';
 import 'package:contability/main_stances.dart';
 import 'package:contability/modules/home/store/states.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
 import '../../../entities/user.dart';
 
-class HomeStore extends ValueNotifier<HomeState>{
+class HomeStore extends ValueNotifier<HomeState> {
   HomeStore(super.value);
 
-  void getDataFromDb()async{
-    List<String> dataList = MainStances.prefs.getStringList('credtCardList')!;
-   List<CreditCard> credtCardList = dataList.map((e) => CreditCard.fromJsonEncodedString(e)).toList();
-   double totalMonth = 0;
-    for (var element in credtCardList) {
-      totalMonth+= element.getTotalOfMonth();
+  void getDataFromDb() async {
+    value = HomeLoading();
+    try {
+      List<String> dataList = MainStances.prefs.getStringList('credtCardList')!;
+      List<CreditCard> credtCardList =
+          dataList.map((e) => CreditCard.fromJsonEncodedString(e)).toList();
+      double totalMonth = 0;
+      for (var element in credtCardList) {
+        totalMonth += element.getTotalOfMonth();
+      }
+      value = HomeData(
+        items: credtCardList,
+        total: totalMonth,
+        leftover: totalMonth - User.salary,
+        motivationalPhrase:
+            '“Se você quer ser bem sucedido, precisa ter dedicação total, buscar seu último limite e dar o melhor de si.” Ayrton Senna',
+      );
+    } catch (e, _) {
+      if (kDebugMode) {
+        print(e);
+        print(_);
+      }
+      value =
+          HomeFailed(errorModel: ErrorModel(msg: 'Erro ao trazer os dados'));
     }
-   value = HomeData(
-     items:  credtCardList,
-     total:totalMonth,
-     leftover: totalMonth - User.salary,
-     motivationalPhrase: '“Se você quer ser bem sucedido, precisa ter dedicação total, buscar seu último limite e dar o melhor de si.” Ayrton Senna',
-   );
   }
 }
