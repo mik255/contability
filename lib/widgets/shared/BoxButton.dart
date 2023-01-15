5b1a9e0fc412cf8caeb97a23c30944c4685e3790
Mik255 <mikael20informatica@gmail.com>
2023-01-15 14:47:26 -0300

    box button

---
+++ lib/widgets/shared/BoxButton.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class BoxButton extends StatelessWidget {
+  const BoxButton({Key? key, required this.text, required this.onTap})
+      : super(key: key);
+  final String text;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 48,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: const Color(0xff00bb6d),
+        ),
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: const TextStyle(
+                color: Color(0xffd8e1e8),
+                fontSize: 16,
+                fontFamily: "Lato",
+                fontWeight: FontWeight.w700,
+                letterSpacing: 1,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
